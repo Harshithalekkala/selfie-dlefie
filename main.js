12e8ff98973b8c68c8d6933d58a7552e25011098
Harshithalekkala <80406614+Harshithalekkala@users.noreply.github.com>
2021-10-25 20:34:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,5 +9,22 @@ recognition.onresult=function run(event){
     console.log(event);
     var content=event.results[0][0].transcript;
     document.getElementById("text-box").innerHTML=content;
+    speak();
 }
 
+function speak() {
+   var synth=window.SpeechSynthesis;
+   speak_data=document.getElementById("text-box").value;
+   var utterthis=new SpeechSynthesisUtterance(speak_data);
+   synth.speak(utterthis);
+   Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
